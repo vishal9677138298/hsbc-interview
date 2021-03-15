7bf98e79a9db2c863b057b5ca393747d532f10ee
vishals <vishal9677138298@gmail.com>
2021-03-15 11:01:54 +0530

    Added reporting plugin

--- src/test/java/TestRunner.java
+++ src/test/java/TestRunner.java
@@ -3,6 +3,9 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/resources/features")
+@CucumberOptions(
+        features = "src/test/resources/features"
+        ,plugin = {"pretty", "html:target/cucumber-reports/execution-results.html"}
+        )
 public class TestRunner {
 }
